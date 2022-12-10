bc69f6e0a3b0e4f33abbcfee8b9ef0aa7ab866d3
Zachary Robinson <me@zsrobinson.com>
2022-12-09 23:48:47 -0500

    day09

---
+++ day09/part1.ts
@@ -0,0 +1,41 @@
+const input = await Deno.readTextFile("./input.txt");
+const lines = input.split("\n").filter((line) => line.length > 0);
+
+type pos = { x: number; y: number };
+const tailStates: pos[] = [{ x: 0, y: 0 }];
+const headStates: pos[] = [{ x: 0, y: 0 }];
+
+function withinOne(num1: number, num2: number) {
+  return Math.abs(num1 - num2) <= 1;
+}
+
+for (const line of lines) {
+  const [direction, distance] = line.split(" ");
+
+  // for each move in a set of moves
+  for (let i = 0; i < parseInt(distance); i++) {
+    const newHead = { ...headStates[0] };
+
+    if (direction === "R") newHead.x++;
+    else if (direction === "L") newHead.x--;
+    else if (direction === "U") newHead.y++;
+    else if (direction === "D") newHead.y--;
+
+    headStates.unshift(newHead);
+
+    // if new head is within one of the tail, continue
+    if (
+      !(
+        (
+          withinOne(newHead.x, tailStates[0].x) &&
+          withinOne(newHead.y, tailStates[0].y)
+        ) /* ||
+        i === parseInt(distance) - 1 */
+      )
+    ) {
+      tailStates.unshift({ ...headStates[1] });
+    }
+  }
+}
+const tailStatesStrings = tailStates.map((state) => JSON.stringify(state));
+console.log([...new Set(tailStatesStrings)].length);
---
+++ day09/part2.ts
@@ -0,0 +1,4 @@
+// I don't have the energy for this rn...
+// My part1 solution can't be too easily modified to work for part2 :(
+// I mean it can but it's not as simple as just changing a variable
+//   and tweaking a few things.
